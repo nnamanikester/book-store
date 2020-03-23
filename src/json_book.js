b7cc0f819c492f1073af0fd8704a617dfb502432
[nwagu Victor ] <[leofizzy3@gmail.com]>
2020-03-23 14:23:06 +0100

    added the products component

---
+++ src/json_book.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+        "id": 1,
+        "bookName": "The Morgan River1",
+        "author": "the author1",
+        "prize": "$ 200",
+        "publishedDate": "20-02-2020"
+    },
+    {
+        "id": 2,
+        "bookName": "The Morgan River2",
+        "author": "the author2",
+        "prize": "$ 200",
+        "publishedDate": "20-02-2020"
+    },
+    {
+        "id": 4,
+        "bookName": "The Morgan River3",
+        "author": "the author3",
+        "prize": "$ 200",
+        "publishedDate": "20-02-2020"
+    }
+]
+
+export default books
