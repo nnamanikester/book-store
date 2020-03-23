653dcd07796e822e31d165af7403f06601397976
[nwagu Victor ] <[leofizzy3@gmail.com]>
2020-03-23 15:19:44 +0100

    added router links to the sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import image from "../images";
+import {NavLink} from 'react-router-dom'
 
 function Sidebar() {
     return (
@@ -10,31 +11,11 @@ function Sidebar() {
                 </div>
                 <div className="menu">
                     <ul>
-                    <li>
-                        <a className="link active" href="/">
-                        Home
-                        </a>
-                    </li>
-                    <li>
-                        <a className="link" href="/">
-                        About
-                        </a>
-                    </li>
-                    <li>
-                        <a className="link" href="/">
-                        Services
-                        </a>
-                    </li>
-                    <li>
-                        <a className="link" href="/">
-                        DMCA
-                        </a>
-                    </li>
-                    <li>
-                        <a className="link" href="/">
-                        Post Book
-                        </a>
-                    </li>
+                        <li><NavLink activeStyle={{color:'red'}} exact to="/">Home</NavLink></li>
+                        <li><NavLink activeStyle={{color:'red'}} exact to="/about">About</NavLink></li>
+                        <li><NavLink activeStyle={{color:'red'}} exact to="/services">Services</NavLink></li>
+                        <li><NavLink activeStyle={{color:'red'}} exact to="/dmca">DMCA</NavLink></li>
+                        <li><NavLink activeStyle={{color:'red'}} exact to="/post_book">Post Book</NavLink></li>
                     </ul>
                 </div>
             </div>
